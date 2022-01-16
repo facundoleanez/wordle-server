1984568ca2b287ae07b38ef8eeaeeb37fcbaee9c
Facundo Leañez <facundo.leanez@hotmail.com>
2022-01-16 17:23:38 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
@@ -6,6 +7,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 //middlewares funciones que se ejecutan antes de llegar a las rutas
+app.use(cors());
 app.use(morgan("dev")); //muestra las peticiones y mensajes que manda el servidor
 app.use(express.json()); //cada vez que se reciba o envie dato vamos a poder usarlos en la api
 
