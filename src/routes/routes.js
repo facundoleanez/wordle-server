af02cce9d99e2695f46a2dd9ce505cc40b4f0fd7
Facundo Leañez <facundo.leanez@hotmail.com>
2022-01-16 17:16:19 -0300

    heroku aded

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
-router.get("/s", (req, res) => {
-  res.json("Hello world");
+  res.json({
+    word: "house",
+    len: 5,
+    report: 0,
+  });
 });
 
 module.exports = router;
